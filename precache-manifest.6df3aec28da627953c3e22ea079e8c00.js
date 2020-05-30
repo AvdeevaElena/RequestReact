261a850d549437731b79fed91130c76709a327f6
avdeevaelena5@gmail.com <avdeevaelena5@gmail.com>
2020-05-30 15:10:54 +0300

    Updates

---
+++ precache-manifest.6df3aec28da627953c3e22ea079e8c00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09524b25ff1958b9427735a7d870b29f",
+    "url": "/RequestReact/index.html"
+  },
+  {
+    "revision": "39170a7c9fdbf4f5b063",
+    "url": "/RequestReact/static/css/2.658eb2da.chunk.css"
+  },
+  {
+    "revision": "635881be40ebe244afed",
+    "url": "/RequestReact/static/css/main.f5915b76.chunk.css"
+  },
+  {
+    "revision": "39170a7c9fdbf4f5b063",
+    "url": "/RequestReact/static/js/2.a774d1cb.chunk.js"
+  },
+  {
+    "revision": "a5e5fdac71758042d8ea9daa6de78e15",
+    "url": "/RequestReact/static/js/2.a774d1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635881be40ebe244afed",
+    "url": "/RequestReact/static/js/main.a530c19f.chunk.js"
+  },
+  {
+    "revision": "5819fed139751451c625",
+    "url": "/RequestReact/static/js/runtime-main.b156ab3b.js"
+  },
+  {
+    "revision": "d6e583ac16b7463e5f3b9f03c7f62165",
+    "url": "/RequestReact/static/media/booksShelfs.d6e583ac.png"
+  }
+]);
